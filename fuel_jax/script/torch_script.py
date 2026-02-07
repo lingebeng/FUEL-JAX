30cb3675819d7e93e655b2328a8a4991fbd8e111
linhaifeng <1371675203@qq.com>
2026-02-07 22:08:27 +0800

    [Feat] implement the torch_script(cpu is ok)

--- fuel_jax/script/torch_script.py
+++ fuel_jax/script/torch_script.py
@@ -1,66 +1,73 @@
-import jax.lax as lax
-import jax
-import jax.numpy as jnp  # noqa: F401
+import torch
 import typer
 import numpy as np
 from pathlib import Path
 from loguru import logger
 from ..config.config import PRECISION_MAP, ROOT_DIR
-from ..utils.utils import save_npz, load_npz, ndarray2Array, Array2ndarray, get_op_key
+from ..utils.utils import (
+    save_npz,
+    load_npz,
+    ndarray2tensor,
+    tensor2ndarray,
+    get_torch_device,
+    get_op_key,
+)
 
 
 def main(
-    op_name: str = typer.Option(..., help="JAX op name"),
+    op_name: str = typer.Option(..., help="PyTorch op name"),
     input_file: Path = typer.Option(None, help="Input file path"),
     output_file: Path = typer.Option(None, help="Output file path"),
     precision: str = typer.Option(
         "FP32", help="Precision setting(FP32, BF16, FP8_E4M3, FP8_E5M2)"
     ),
-    device: str = typer.Option("cpu", help="Device setting(cpu, gpu or tpu)"),
-    compile_flag: bool = typer.Option(False, help="Enable JIT compilation(xla or not)"),
+    device: str = typer.Option("cpu", help="Device setting(cpu or gpu)"),
+    compile_flag: bool = typer.Option(
+        False, help="Enable Inductor compilation(inductor or not)"
+    ),
 ):
     if not input_file:
         input_file = ROOT_DIR / "input" / get_op_key(op_name) / "00.npz"
-
     if not output_file:
         output_file = (
             ROOT_DIR
             / "output"
             / get_op_key(op_name)
             / precision
-            / f"out_jax_{device}.npz"
+            / f"out_torch_{device}.npz"
         )
-
     # Load input data
     inp = load_npz(input_file)
     # Convert input data to JAX array with specified precision
-    dtype_str = PRECISION_MAP[precision]["jax"]
+    dtype_str = PRECISION_MAP[precision]["torch"]
 
-    target_device = jax.devices(device)[0]
-
-    for k, v in inp.items():
+    device = get_torch_device(device=device)
+    inp_lst = []
+    for v in inp.values():
         if isinstance(v, np.ndarray):
-            inp[k] = jax.device_put(
-                ndarray2Array(v, dtype=eval(dtype_str)), device=target_device
-            )
+            x = ndarray2tensor(v, dtype=eval(dtype_str)).to(device)
+            inp_lst.append(x)
+        else:
+            inp_lst.append(v)
 
     # Define the JAX function to compute absolute values
-    fn = getattr(lax, op_name)
+    fn = getattr(torch, op_name)
 
     try:
         save_kwargs = {}
-        out = Array2ndarray(fn(**inp))
-        save_kwargs[f"out_jax_{device}"] = out
+        out = tensor2ndarray(fn(*inp_lst))
+        save_kwargs[f"out_torch_{device}"] = out
         if compile_flag:
-            fn_compile = jax.jit(fn)
-            out_jit = Array2ndarray(fn_compile(**inp))
-            save_kwargs[f"out_jax_xla_{device}"] = out_jit
+            fn_compile = torch.compile(fn)
+
+            out_jit = tensor2ndarray(fn_compile(*inp_lst))
+            save_kwargs[f"out_torch_inductor_{device}"] = out_jit
 
         if save_kwargs:
             save_npz(output_file, **save_kwargs)
         exit(0)
     except Exception as e:
-        logger.error(f"Error executing {op_name} on JAX: {e}")
+        logger.error(f"Error executing {op_name} on PyTorch: {e}")
         exit(1)
 
 
